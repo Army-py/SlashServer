9d7f1c05b68a9617615cec59e52c823d6e9ecf92
Army-py <61599555+Army-py@users.noreply.github.com>
2023-06-12 01:17:59 +0200

    Change 'proxy' to private access in SlashServer.java

--- src/main/java/com/minecraftdimensions/slashserver/SlashServer.java
+++ src/main/java/com/minecraftdimensions/slashserver/SlashServer.java
@@ -13,7 +13,7 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class SlashServer extends Plugin {
-    ProxyServer proxy;
+    private ProxyServer proxy;
 
     public static Config config;
 
